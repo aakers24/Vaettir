d4ff35d96ae268fe4b783379f20aaa8bd3c701b8
aakers24 <AustinA.Akers97@gmail.com>
2026-06-17 22:07:05 -0700

    Bug Fix diagnostic colors absolute anotha one again again

--- script.js
+++ script.js
@@ -217,7 +217,7 @@ function generateHeader(name, link) {
 }
 
 // DEFINE HARDCODED HEADERS
-headers.push(generateHeader("Vaettir2", "EotN"));
+headers.push(generateHeader("Vaettir3", "EotN"));
 
 function drawHeaders(){
     headers.forEach((header) => {
